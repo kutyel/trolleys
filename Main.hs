f1f4812ed6c9834f47edfdadbef296489e15958d
kutyel <flaviocorpa@gmail.com>
2019-10-20 18:56:35 +0200

    add random stuff

--- Main.hs
+++ Main.hs
@@ -1,12 +1,28 @@
 module Main where
 
+import           Data.Maybe
+import           System.Random
+import           System.Random.Shuffle (shuffleM)
+
 type Shift = String
-type Shifts = [Shift] 
-type Shedule = [Turn]
 
-data Turn = Empty | Captain Volunteer | Volunteers [Volunteer]
+type Shifts = [Shift]
 
-data Volunteer = Volunteer { name :: String, availability :: Shifts }
+type Schedule = [Turn]
+
+data Turn
+  = Empty
+  | Captain Volunteer
+  | Volunteers [Volunteer]
+
+data Volunteer =
+  Volunteer
+    { name         :: String
+    , availability :: Shifts
+    }
+
+fillTheGaps :: Shifts -> [Volunteer] -> Schedule
+fillTheGaps = undefined -- TODO:
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
