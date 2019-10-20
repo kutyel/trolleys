8eaf1bf99bc0b2c0ab9e81acac1bb8eccc4ae5ab
kutyel <flaviocorpa@gmail.com>
2019-10-20 19:54:41 +0200

    Implement getVolunteers fn

--- Main.hs
+++ Main.hs
@@ -20,9 +20,24 @@ data Volunteer =
     { name         :: String
     , availability :: Shifts
     }
+  deriving (Show)
+
+getVolunteers :: Shift -> [Volunteer] -> [Volunteer]
+getVolunteers s = filter $ elem s . availability
 
 fillTheGaps :: Shifts -> [Volunteer] -> Schedule
 fillTheGaps = undefined -- TODO:
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
+
+-- Try it out!
+turns :: Shifts
+turns = ["M1", "M2", "M3"]
+
+vols :: [Volunteer]
+vols =
+  [ Volunteer "Flavio" ["M1", "M2", "M3"]
+  , Volunteer "Lydia" ["M2", "M3"]
+  , Volunteer "Eva" ["M3"]
+  ]
